a1916cd30ec392b62ffc8e62f52ed9ff285dc573
jackup <yk.58@hotmail.com>
2021-12-07 18:58:13 +0300

    Add project files.

---
+++ MCD_DictionarySortedListCollection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_DictionarySortedListCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generic olmayan koleksiyon davranışı..
+            Hashtable htList = new Hashtable();
+            htList.Add(1, "Bir");
+            htList.Add(2, "İki");
+            htList.Add(3, "Üç");
+            //htList.Add(1, "Test");
+
+            //Dictionary<Tkey, TValue>
+            Dictionary<int, string> dList = new Dictionary<int, string>();
+            dList.Add(1, "Bir");
+            dList.Add(2, "İki");
+            dList.Add(3, "Üç");
+            //dList.Add(3, "Test");
+
+        }
+    }
+}
